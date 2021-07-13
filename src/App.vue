edc424f855e73018911ca7fa320df990a6fe2067
Allison Broughton <purplelady105@gmail.com>
2021-07-12 19:55:56 -0700

    Set up top menu

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,71 @@
 <template>
   <v-app>
+    <font-awesome-icon icon="user-secret" />
     <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+     app
+     color="primary"
+     dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <router-link to="/" class="header-logo">
+          <v-img :src="require('@/assets/logo.png')" alt="PV Pest Logo" width="85" contain></v-img>
+        </router-link>
       </div>
-
       <v-spacer></v-spacer>
-
+      <router-link to="/pay">
+        <v-btn
+         text
+         color="softGrey">
+          Pay
+        </v-btn>
+      </router-link>
+      <router-link to="/pay">
+        <v-btn
+         text
+         color="softGrey">
+          Service Areas
+        </v-btn>
+      </router-link>
+      <router-link to="/about">
+        <v-btn
+         text
+         color="softGrey">
+          About
+        </v-btn>
+      </router-link>
+      <router-link to="/about">
+        <v-btn
+         text
+         color="softGrey">
+          Contact
+        </v-btn>
+      </router-link>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+       icon
+       color="white">
+        <v-icon>fab fa-fw fa-instagram</v-icon>
+      </v-btn>
+      <v-btn
+       icon
+       color="white">
+        <v-icon>fab fa-fw fa-facebook-f</v-icon>
+      </v-btn>
+      <v-btn
+       href="https://github.com/vuetifyjs/vuetify/releases/latest"
+       target="_blank"
+       color="accent"
+       outlined
+       rounded
+       elevation="5"
+       medium>
+        <span class="mr-2">Call Now</span>
       </v-btn>
     </v-app-bar>
-
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
-
 <script>
-
 export default {
   name: 'App',
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,24 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	icons: {
+		iconfont: 'fa' || 'mdi' // initialize Font Awesome as the default font for icons
+	},
+	theme: {
+		themes: {
+			light: {
+				primary: '#000000',
+				secondary: '#F6F6F6',
+				accent: '#BC9C4E',
+				error: '#f44336',
+				warning: '#ff5722',
+				info: '#4caf50',
+				success: '#03a9f4',
+				softGrey: '#E6E2DB'
+			},
+			dark: {
+				primary: '#34495E'
+			}
+		}
+	}
 });
