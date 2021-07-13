7ac0a41700fdcbac5cc33d111c8527f0be10d695
Allison Broughton <purplelady105@gmail.com>
2021-07-13 16:09:10 -0700

    Add footer and call to action. Increased padding between components.

--- src/App.vue
+++ src/App.vue
@@ -57,19 +57,68 @@
       </v-btn>
       <v-btn
        href="tel:+16024921056"
-       target="_blank"
        color="accent"
        outlined
        rounded
        elevation="5"
        class="mt-2"
        medium>
-        <span class="mr-2">Call Now</span>
+        Call Now
       </v-btn>
     </v-app-bar>
     <v-main class="mt-16 primary">
       <router-view />
     </v-main>
+    <v-card flat tile width="100%" class="accent white--text text-center pa-5">
+      <div class="text-h3 font-weight-bold py-6">Schedule service today!</div>
+      <v-btn
+       href="mailto:info@pvpest.com?subject=Pest Control Inquiry"
+       color="white"
+       outlined
+       rounded
+       elevation="5"
+       class="my-10 pa-9">
+        <span class="text-h3">Contact Us</span>
+      </v-btn>
+    </v-card>
+    <v-footer dark padless>
+      <v-card flat tile width="100%" class="darkGrey white--text text-center pt-16">
+        <v-card-text class="pb-16 text-left text-md-center">
+          <v-layout>
+            <div class="col-md-4 col-12 elevation-0">
+              <div class="text-h6 font-weight-bold">Location</div>
+              <div class="text-h6 mt-5">
+                9950 W. Van Buren Street Suite 125<br>
+                Avondale, AZ 85323
+              </div>
+            </div>
+            <!-- <v-divider vertical></v-divider> -->
+            <div class="col-md-4 col-12 elevation-0">
+              <div class="text-h6 font-weight-bold">Hours</div>
+              <div class="text-h6 mt-5">
+                Monday — Friday<br>7am — 5pm
+                <v-divider></v-divider>
+                Saturday<br>7am — 4pm
+                <v-divider></v-divider>
+                Sunday<br>Closed
+              </div>
+            </div>
+            <!-- <v-divider vertical></v-divider> -->
+            <div class="col-md-4 col-12 elevation-0">
+              <div class="text-h6 font-weight-bold">Contact</div>
+              <div class="text-h6 mt-5">
+                <a href="mailto:info@pvpest.com?subject=Pest Control Inquiry" class="text-decoration-none softGrey--text">info@pvpest.com</a><br>
+                <a href="tel:+16024921056" class="text-decoration-none softGrey--text">(602) 492-1056</a>
+              </div>
+            </div>
+          </v-layout>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>PV Pest Solutions</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-container>
-    <v-row class="text-left white--text">
+  <div class="my-16">
+    <v-row class="text-left white--text mx-15">
       <span class="text-h1 font-weight-bold">Serving The West Valley & Maricopa County</span>
       <span class="text-h2 font-weight-bold mt-6">We Offer Guaranteed Year Round Residential Pest Protection.</span>
     </v-row>
-    <v-row>
+    <!-- ABOUT US SECTION -->
+    <v-row class="mx-15 pt-16">
       <v-card
        class="transparent my-16"
        flat>
@@ -13,19 +14,20 @@
             <v-img :src="require('@/assets/white-square-house.jpg')" max-height="50vh"></v-img>
           </v-flex>
           <v-flex xs6>
-            <v-card-title primary-title class="white--text">
+            <v-card-title primary-title class="white--text pl-16">
               <div>
                 <span class="text-h3">About Us</span>
               </div>
             </v-card-title>
-            <v-card-text class="white--text">
+            <v-card-text class="white--text pl-16">
               <span class="text-h4">PV Pest Solutions is proud to offer residential & commercial pest control services to the residents of Goodyear, Avondale, & Litchfield Park AZ.</span>
             </v-card-text>
           </v-flex>
         </v-layout>
       </v-card>
     </v-row>
-    <v-row justify="space-around" class="my-16">
+    <!-- FIRST ROW OF PEST AVATARS -->
+    <v-row justify="space-around" class="py-16 mt-16 mb-0 softGrey">
       <v-avatar
        size="300"
        @click="openTriviaDialog(0)">
@@ -42,7 +44,8 @@
         <v-img :src="require('@/assets/black-widow.jpg')"></v-img>
       </v-avatar>
     </v-row>
-    <v-row justify="space-around" class="my-16">
+    <!-- SECOND ROW OF PEST AVATARS -->
+    <v-row justify="space-around" class="pb-16 mt-0 mb-16 softGrey">
       <v-avatar
        size="300"
        @click="openTriviaDialog(3)">
@@ -59,6 +62,32 @@
         <v-img :src="require('@/assets/spider.jpg')"></v-img>
       </v-avatar>
     </v-row>
+    <!-- CUSTOMER TESTIMONIALS -->
+    <v-row class="mx-15">
+      <v-card
+       class="transparent my-16"
+       flat>
+        <v-layout>
+          <v-flex xs6>
+            <v-card-text class="white--text">
+              <div>
+                <span class="text-h4 text-wrap">"We are so happy with PV Pest Solutions! Their commitment to providing outstanding customer service is unmatched."</span>
+              </div>
+              <span class="text-h5">- Paula G.</span>
+            </v-card-text>
+          </v-flex>
+          <v-flex xs6>
+            <v-card-text class="white--text">
+              <div>
+                <span class="text-h4">"Excellent service. Our technician Joe was very thorough and made sure all of our concerns were addressed. Would highly recommend."</span>
+              </div>
+              <span class="text-h5">- Deborah S.</span>
+            </v-card-text>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-row>
+    <!-- TRIVIA DIALOG POPUP -->
     <v-dialog
      v-model="triviaDialog"
      width="500px">
@@ -79,7 +108,7 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-  </v-container>
+  </div>
 </template>
 <script>
 export default {
