9ccfc015e62bb7a7e45992ff24dc769f962e9f4f
Allison Broughton <purplelady105@gmail.com>
2021-07-20 13:58:23 -0700

    Add promo video to About Us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="mt-0 mb-16">
-    <v-card flat tile width="100%" class="transparent white--text text-center">
-      <div class="text-h2 py-6">About Us</div>
-    </v-card>
-  </div>
+	<div class="mt-0 mb-16">
+		<v-card flat tile width="100%" class="transparent white--text text-center">
+			<div class="text-h2 py-6">About Us</div>
+			<video width="70%" height="30%" controls>
+				<source :src="require('@/assets/video.mp4')" type="video/mp4">
+				Your browser does not support the video tag.
+			</video>
+		</v-card>
+	</div>
 </template>
